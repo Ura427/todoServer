4eec9755e864c2cf9ebf4c22d8bcd4bf26ebad5a
Ura427 <urakost427@gmail.com>
2024-02-13 16:50:21 +0200

    use cors

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
-
+const cors = require("cors");
 const PORT = 5000;
 const todoRouter = require("./routes/todos_router");
 
 const app = express();
 
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
 app.use(express.json());
 app.use("/api", todoRouter);
-
 
 app.listen(PORT, () => console.log(`Listen to http://localhost:${PORT}`));
